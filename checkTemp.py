372d4115addfaccf0895b5ab8eb213663bec9f6a
zayanthepatel <zayanpatel2004@gmail.com>
2023-11-28 09:48:45 +0400

    second commit

---
+++ checkTemp.py
@@ -0,0 +1,7 @@
+import myTemp as t
+c=int(input("Enter your temp in degree C and we will provide the F Temp"))
+
+print(c, "Celcuis is equivalent to", t.ctf(c))
+
+f=int(input("Enter your temp in degree F and we will provide the C Temp"))
+print(f, "Celcuis is equivalent to", t.ftc(f))
---
+++ myTemp.py
@@ -0,0 +1,6 @@
+def ctf (ctemp):
+    ftemp=9/5*ctemp+32
+    return ftemp
+def ftc (ftemp):
+    ctemp= (ftemp-32)*5/9
+    return ctemp
